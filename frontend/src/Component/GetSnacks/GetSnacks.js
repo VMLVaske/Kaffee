ad00daeaa4698612aa74fa8279b2883254247a2b
Valerie <valerie.vaske@gmail.com>
2019-07-19 16:04:47 +0200

    Einfach nur ausprobiert, funktioniert nicht

--- frontend/src/Component/GetSnacks/GetSnacks.js
+++ frontend/src/Component/GetSnacks/GetSnacks.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import './Style.css';
 
-class Login extends React.Component {
-  render() {    
-    return <div>
-      Bitte den Mitarbeiterausweis vor den NFC Reader halten.
-    </div>;
+class GetSnacks extends React.Component {
+
+  componentDidMount() {
+    var url = "https://jsonplaceholder.typicode.com/posts"
+    fetch({url})
+    .then( response => response.json() )
+    .then(
+      //handle the result
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          posts: result
+        });
+      },
+
+      // Handle Error
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      },
+    )
+  }
+  render() {
+    const {error, isLoaded, posts} = this.state;
+    if(error) {
+      return <div>Error in Loading </div>
+    } else if(!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return(
+        <div>
+          <ol>
+          {
+            posts.map(post=> (
+              <li key={post.id} align="start">
+                <div>
+                  <p>{post.title}</p>
+                  <p>{post.body}</p>
+                </div>
+              </li>
+            ))
+          }
+          </ol>
+        </div>
+      );
+    }
   }
 }
 
-export default Login;
+export default GetSnacks;
