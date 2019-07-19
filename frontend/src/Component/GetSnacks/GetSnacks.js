fa3405bd2bf771fcec5b0dc57b225f2b9fea07b7
Valerie <valerie.vaske@gmail.com>
2019-07-19 15:09:44 +0200

    Weiterführende Components hinzugefügt

---
+++ frontend/src/Component/GetSnacks/GetSnacks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Style.css';
+
+class Login extends React.Component {
+  render() {    
+    return <div>
+      Bitte den Mitarbeiterausweis vor den NFC Reader halten.
+    </div>;
+  }
+}
+
+export default Login;
---
+++ frontend/src/Component/Snacks/Snacks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Style.css';
+
+class Snacks extends React.Component {
+  render() {
+    return (
+      <div>
+      hier sind die Snacks!
+      </div>
+    );
+  }
+}
+
+export default Snacks;
