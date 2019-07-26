7888a9356c6361e1c21f448bf8f36d77499770aa
Valerie <valerie.vaske@gmail.com>
2019-07-26 16:29:20 +0200

    Changes in App Component

--- frontend/src/Component/App/App.js
+++ frontend/src/Component/App/App.js
@@ -4,17 +4,17 @@ import './Style.css';
 
 import Login from './../Login/Login';
 import Register from './../Register/Register';
+import Snacks from './../Snacks/Snacks';
 
 // JSON Experimente
-import GetOnlinePosts from './../OnlinePosts/GetOnlinePosts';
-import GetLocalPosts from './../LocalPosts/GetLocalPosts';
+// import GetOnlinePosts from './../OnlinePosts/GetOnlinePosts';
+// import GetLocalPosts from './../LocalPosts/GetLocalPosts';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isToggleOn: false,
-      isRegistered: false
+      isLoggedIn: false
     };
 
     // This binding is necessary to make 'this' work in the callback
@@ -33,39 +33,47 @@ class App extends React.Component {
     }
   }
 
+  isInJSON() {
+    this.setState({
+      isLoggedIn: !this.state.isLoggedIn
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
 
-          <h2> Willkommen in der digitalen Kaffeekasse" </h2>
-
-          <div className="ButtonContainer">
-            <button className="LoginButton" onClick={this.handleClick.bind(this, 'login')} block>
-              Einloggen
-            </button>
-            <button className="RegisterButton" onClick={this.handleClick.bind(this, 'register')} block>
-              Registrieren
-            </button>
-            {this._renderSubComp()}
-          </div>
-        </header>
-
-        <body className="StorageBody">
-          <h1 className="localStorage">Local Storage</h1>
-          <GetLocalPosts/>
-          <br/>
-          <h1 className="externalStorage">External Storage</h1>
-          <GetOnlinePosts/>
-        </body>
-
-
-        <footer className="App-footer">
-        A student project by Felix & Valerie.
-        </footer>
-      </div>
-
+    return(
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2> Willkommen im Snack-Land </h2>
+          </header>
+          <navbar className="App-navbar">
+            <div className="ButtonContainer">
+              <button className="LoginButton" onClick={this.handleClick.bind(this, 'login')}>
+                Einloggen
+              </button>
+              <button className="RegisterButton" onClick={this.handleClick.bind(this, 'register')}>
+                Registrieren
+              </button>
+              {/*
+                <button className="ShowInternalJson" onClick={this.handleClick.bind(this, 'ShowInternalJson')}>
+                Show Internal JSON
+              </button>
+              */}
+              {/*
+              <button className="ShowExternalJson" onClick={this.handleClick.bind(this, 'ShowExternalJson')}>
+                Show External JSON
+              </button>
+              */}
+              {this._renderSubComp()}
+              </div>
+          </navbar>
+          <body className="App-body">
+          </body>
+          <footer className="App-footer">
+          A student project by Felix & Valerie.
+          </footer>
+        </div>
     );
   }
 }
